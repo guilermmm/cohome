8df59e8b0141904403f4ef400678df0518166820
guilermmm <guilherme.r.a.melo@gmail.com>
2023-08-21 23:22:44 -0300

    validation and removed birthdate

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,6 +1,9 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
 export class CreateUserDto {
+  @IsNotEmpty()
   name: string;
+  @IsEmail()
   email: string;
+  @IsNotEmpty()
   password: string;
-  birthdate: Date;
 }
