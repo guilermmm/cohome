83b513c063a2b12f56cd7131cb6f54ea8fc79f49
guilermmm <guilherme.r.a.melo@gmail.com>
2023-08-29 09:03:40 -0300

    seinao

--- client/src/pages/groups.tsx
+++ client/src/pages/groups.tsx
@@ -1,89 +1,87 @@
-import Button from "@/components/Button";
-import Input from "@/components/Input";
-import NavBar from "@/components/NavBar";
-import {
-  deleteGroup,
-  getGroup,
-  getOneGroup,
-  postGroup,
-} from "@/services/routes/group";
-import { getOneUser, getUser } from "@/services/routes/user";
-import axios from "axios";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { FormEvent, useState } from "react";
-import { useMutation, useQuery } from "react-query";
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+import NavBar from '@/components/NavBar'
+import { deleteGroup, getOneGroup, postGroup } from '@/services/routes/group'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { FormEvent, useEffect, useState } from 'react'
+import { useMutation, useQuery } from 'react-query'
 
 export default function Groups() {
-  const router = useRouter();
-  if (typeof window !== "undefined" && !localStorage.getItem("token")) {
-    router.push("/login");
+  const router = useRouter()
+  if (typeof window !== 'undefined' && !localStorage.getItem('token')) {
   }
 
-  const [name, setName] = useState("");
+  const [name, setName] = useState('')
+  const [userId, setUserId] = useState('')
 
-  const userId = (localStorage?.getItem("userId") as string) ?? "";
+  useEffect(() => {
+    setUserId((localStorage?.getItem('userId') as string) ?? '')
+
+    if (!localStorage.getItem('token')) router.push('/')
+  }, [router])
 
   const group = useQuery({
-    queryKey: ["group"],
+    queryKey: ['group'],
     queryFn: () => getOneGroup(userId as string),
     enabled: userId !== undefined,
-  });
+  })
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    e.preventDefault()
     createGroup.mutate({
       name,
       userId,
-    });
-  };
+    })
+  }
 
   const createGroup = useMutation({
     mutationFn: postGroup,
     onSuccess: () => {
-      router.push("/groups");
+      group.refetch()
     },
-    onError: (e) => {
+    onError: e => {
       if (axios.isAxiosError(e)) {
-        alert("Falha no cadastro: " + e.response?.data.message);
+        alert('Falha no cadastro: ' + e.response?.data.message)
       }
     },
-  });
+  })
 
   const removeGroup = useMutation({
     mutationFn: deleteGroup,
     onSuccess: () => {
-      group.data?.data.users.pop();
-      router.push("/");
+      setName('')
+      group.remove()
+      group.refetch()
     },
-    onError: (e) => {
+    onError: e => {
       if (axios.isAxiosError(e)) {
-        alert("Falha na remoção: " + e.response?.data.message);
+        alert('Falha na remoção: ' + e.response?.data.message)
       }
     },
-  });
+  })
 
   return (
     <div className="h-screen w-screen bg-gray-300">
       <NavBar />
 
-      {group.data?.data.users[0]?.id == userId ? (
+      {group.data?.data.users ? (
         <div className=" m-10 rounded-md bg-white flex flex-col gap-3 p-4">
           <div className="flex justify-between">
-            {" "}
+            {' '}
             <h1 className="text-cyan-800 font-bold text-xl">
               Membros do grupo {group.data.data.name}
             </h1>
             <Button
               color="red"
-              text="Sair do grupo"
+              text="Excluir grupo"
               onClick={() => removeGroup.mutate(group.data?.data.id as string)}
             />
           </div>
 
           <div className="bg-gray-300 w-9/10 rounded p-2 flex justify-between">
             <h1 className="text-gray-600 font-bold text-md mr-4 truncate">
-              {"Administrador:  " + group.data.data.users[0].name}
+              {'Administrador:  ' + group.data.data.users[0].name}
             </h1>
           </div>
 
@@ -105,5 +103,5 @@ export default function Groups() {
         </div>
       )}
     </div>
-  );
+  )
 }
